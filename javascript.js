232ab14e778f1f82998780fd558cdb5ac9cbd6f0
benzinho75 <97792463+benzinho75@users.noreply.github.com>
2023-09-21 15:33:20 +0200

    Implement single round game and return result

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-function getRandomNumber () {
-  return Math.ceil(Math.random() * 3);
-}
 
-function getComputerChoice (randomNumber) {
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("What is your choice?").toLowerCase();
+
+console.log(computerSelection);
+console.log(playerSelection);
+console.log(playRound(playerSelection, computerSelection))
+
+
+function getComputerChoice () {
+  let randomNumber = Math.ceil(Math.random() * 3)
   let choice  
   if (randomNumber === 1) {
       choice = "rock";
@@ -14,26 +20,22 @@ function getComputerChoice (randomNumber) {
    return choice;
 }
 
-playerSelection = prompt ("What is your choice?").toLowerCase;
-
-
-const randomNumber = getRandomNumber();
-const computerSelection = getComputerChoice(randomNumber);
-console.log(computerSelection);
-console.log(playerSelection);
-
-// function that plays a single round of RPS which takes two parameters: playerSelection and computerSelection and returns string declaring winner like so "You Lose! Paper beats Rock"
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-
-
-// function playRound(playerSelection, computerSelection) {
-    // your code here!
-//  }
-//   
-//  const playerSelection = "rock";
-//  const computerSelection = getComputerChoice();
-//  console.log(playRound(playerSelection, computerSelection));
-
-
-  // Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
+function playRound (playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie"
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats rock!"
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats scissors!"
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats rock!"
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors beat paper!"
+  } else if (playerSelection === "Scissors" && computerSelection === "rock") {
+    return "You win! Rock beats beats scissors!"
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    return "You win! Scissors beat paper!"
+  } else {
+    return "Fatal error"
+  }
+}
